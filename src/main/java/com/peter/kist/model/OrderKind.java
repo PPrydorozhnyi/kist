f8a8381df67c9206b8e6ab803c7aaba126068941
Petro Prydorozhnyi <Peter35128@gmail.com>
2019-10-28 21:06:08 +0200

    added Vadim`s entities

---
+++ src/main/java/com/peter/kist/model/OrderKind.java
@@ -0,0 +1,24 @@
+package com.peter.kist.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "pr_order_kind")
+public class OrderKind {
+
+    @Id
+    @Column(name = "order_kind_id", updatable = false, nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Length(max = 50)
+    @Column(name = "order_kind_name")
+    private String name;
+
+}
---
+++ src/main/java/com/peter/kist/model/PunishKind.java
@@ -0,0 +1,24 @@
+package com.peter.kist.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "pr_punish_kind")
+public class PunishKind {
+
+    @Id
+    @Column(name = "punish_kind_id", updatable = false, nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Length(max = 50)
+    @Column(name = "punish_kind_name")
+    private String name;
+
+}
