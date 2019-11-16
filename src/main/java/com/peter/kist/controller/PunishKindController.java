a1727e13d6904722e39b9e5b41bfb8ce543fa023
Vadim Gerashchenko <vadim.gerashchenko@gmail.com>
2019-11-16 18:45:08 +0200

    View for punish-kind

---
+++ src/main/java/com/peter/kist/controller/PunishKindController.java
@@ -0,0 +1,90 @@
+package com.peter.kist.controller;
+
+import com.peter.kist.model.PunishKind;
+import com.peter.kist.service.PunishKindService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@Slf4j
+@RequiredArgsConstructor(onConstructor_ = {@Autowired})
+@RequestMapping("/punish-kind")
+public class PunishKindController {
+    private final PunishKindService punishKindService;
+
+    @GetMapping("/{id}")
+    public String getPunishKind(Model model, @PathVariable Integer id) {
+
+        log.debug("getPunishKind");
+
+        PunishKind punishKind = punishKindService.getPunishKind(id);
+
+        model.addAttribute("punishKindForm", punishKind);
+
+        return "punishKindView";
+    }
+
+    @GetMapping("/create")
+    public String createPunishKind(Model model) {
+
+        log.debug("createPunishKind");
+
+        PunishKind punishKind = new PunishKind();
+
+        model.addAttribute("punishKindForm", punishKind);
+
+        return "punishKindCreation";
+    }
+
+    @PostMapping("/create")
+    public String createPunishKind(@ModelAttribute("punishKindForm") PunishKind punishKindForm, BindingResult bindingResult) {
+
+        log.debug("PunishKind creation");
+
+        if (bindingResult.hasErrors()) {
+            return "punishKindCreation";
+        }
+
+        punishKindService.createPunishKind(punishKindForm);
+
+        return "redirect:/punish-kind/" + punishKindForm.getId();
+    }
+
+    @PostMapping("/edit")
+    public String edit(@ModelAttribute("punishKindForm") PunishKind punishKind, Model model) {
+        log.debug("editPunishKind");
+
+        return "punishKindCreation";
+    }
+
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable Integer id) {
+
+        log.debug("deletePunishKind");
+
+        punishKindService.deletePunishKind(id);
+
+        return "redirect:/punish-kind/all";
+    }
+
+    @GetMapping("/all")
+    public String findAll(Model model) {
+
+        log.debug("findAllPunishKind");
+
+        List<PunishKind> punishKind = punishKindService.findAll();
+
+        model.addAttribute("punishKind", punishKind);
+
+        model.addAttribute("deletedUserName", null);
+
+        return "punishKindTableView";
+    }
+}
---
+++ src/main/webapp/punishKindCreation.jsp
@@ -0,0 +1,43 @@
+<%@ taglib prefix="spring" uri="http://www.springframework.org/tags" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
+<c:set var="contextPath" value="${pageContext.request.contextPath}"/>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Punish kind page</title>
+
+    <link href="${contextPath}/resources/css/bootstrap.min.css" rel="stylesheet">
+    <link href="${contextPath}/resources/css/common.css" rel="stylesheet">
+    <link href="${contextPath}/resources/css/my.css" rel="stylesheet">
+</head>
+
+<body>
+
+<div class="container">
+
+    <form:form method="POST" action="${contextPath}/punish-kind/create" modelAttribute="punishKindForm" class="form-signin">
+        <h2 class="form-signin-heading">${punishKindForm.id != null ? 'Edit' : 'Create'} punish kind</h2>
+
+        <form:hidden path="id" />
+
+        <%--NAME--%>
+        <spring:bind path="name">
+            <div class="form-group ${status.error ? 'has-error' : ''}">
+                <form:input type="text" path="name" class="form-control" placeholder="Punish kind name"
+                            autofocus="true"/>
+                <form:errors path="name"/>
+            </div>
+        </spring:bind>
+
+        <button class="btn btn-lg btn-primary btn-block" type="submit">Submit</button>
+    </form:form>
+</div>
+
+<script src="${contextPath}/resources/js/jquery.min.js"></script>
+<script src="${contextPath}/resources/js/bootstrap.min.js"></script>
+</body>
+</html>
+
