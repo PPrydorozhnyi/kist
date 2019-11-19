9a7ba9451ed415e007aec6d7ba15447f3e38c733
pepr0718 <petro.prydorozhnyi@netcracker.com>
2019-11-19 15:30:19 +0200

    added first query

--- src/main/java/com/peter/kist/repository/StudentRepository.java
+++ src/main/java/com/peter/kist/repository/StudentRepository.java
@@ -12,6 +12,13 @@ public interface StudentRepository extends JpaRepository<Student, Integer> {
      * Query for search all students that have marks higher than average for specified subject and group
      */
     @Query(value = "SELECT s FROM Student s JOIN s.groups g JOIN s.marks m JOIN g.teacherPlans t JOIN t.subject subj " +
-            "WHERE g.id = :groupId AND subj.id = :subjectId AND m.value > avg(m.value)")
-    public List<Student> customQuery1(@Param("groupId") Integer groupId, @Param("subjectId") Integer subjectId);
+            "WHERE g.id = :groupId " +
+            "AND subj.id = :subjectId " +
+            "AND m.value > " +
+            "   (select avg(mark.value) " +
+            "       from Student st " +
+            "       join st.groups gr " +
+            "       join st.marks mark " +
+            "       where gr.id = :groupId)")
+    List<Student> customQuery1(@Param("groupId") Integer groupId, @Param("subjectId") Integer subjectId);
 }
